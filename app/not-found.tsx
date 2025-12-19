06d3390e797feff5d09235c4659eb96985f9ab0a
root <root@srv1208815.hstgr.cloud>
2025-12-19 08:20:43 +0000

    update: migrate to new server

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFound() {
       <main className="wrapper fade">
         <h1 className="header">404 - Not Found</h1>
         <p className="mt-4 text-gray-400">
-          Could not find requested resource. Let's go back <Link href={'/'} className="text-white font-medium">home.</Link>
+          Could not find requested resource. Let's go back <Link href={'/'} className="text-white font-medium">home</Link>
         </p>
       </main>
     </MainLayout>
